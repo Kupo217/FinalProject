2433a110722679493891028ecddcedfa1adeb2e6
sophiemamulashvili <sopiko.mamulashvili.1@btu.edu.ge>
2021-02-02 01:10:58 +0400

    პირველი 3 ფეიჯის დიზაინი

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_3.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    fun init(){
+        login.setOnClickListener(){
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        sign_up_txt_view.setOnClickListener(){
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
